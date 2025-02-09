b30408255f16c0c59c3007390a5719d555243d76
Ki Wook Kim <wook.kim@rwth-aachen.de>
2025-02-09 19:49:28 +0100

    first commit

--- DopamineFriends/DopamineFriendsApp.swift
+++ DopamineFriends/DopamineFriendsApp.swift
@@ -4,11 +4,14 @@
 //
 //  Created by joki on 09.02.25.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct DopamineFriendsApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ DopamineFriends/Views/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  DopamineFriends
+//
+//  Created by joki on 09.02.25.
+//
+
+import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                VStack{
+                    Text("DopamineFriends")
+                }
+            }
+        }
+    }
+}
