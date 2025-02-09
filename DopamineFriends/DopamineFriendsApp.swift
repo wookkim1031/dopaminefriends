6a7338e36a598d012db61f95c016da36d4685807
Ki Wook Kim <wook.kim@rwth-aachen.de>
2025-02-09 18:43:07 +0100

    Initial Commit

---
+++ DopamineFriends/DopamineFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DopamineFriendsApp.swift
+//  DopamineFriends
+//
+//  Created by joki on 09.02.25.
+//
+
+import SwiftUI
+
+@main
+struct DopamineFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DopamineFriendsUITests/DopamineFriendsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DopamineFriendsUITestsLaunchTests.swift
+//  DopamineFriendsUITests
+//
+//  Created by joki on 09.02.25.
+//
+
+import XCTest
+
+final class DopamineFriendsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
